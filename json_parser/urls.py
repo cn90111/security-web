da90558c9fa1a54841c77ab02347c3bc2ac9f7b0
bob <cn90111@gmail.com>
2020-04-30 15:36:14 +0800

    perf: 改進解析url的方式
    調整項目：
    1. caller 統一由前往後取，這樣split後取得數字固定是3
    2. 將FileView從json的views中提出，供DPSyn使用
    3. 將CSV_View_output/upload整合為一條
    
    issue #49
    
    連帶解決：
    issue #54
    issue #50

--- json_parser/urls.py
+++ json_parser/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('File_Upload/', views.FileView.as_view()),
-    path('string_pair/csv_name=<str:csv_name>/', views.FileView.as_view()),
+    path('File_Upload/', views.JsonFileView.as_view()),
+    path('string_pair/csv_name=<str:csv_name>/', views.JsonFileView.as_view()),
     path('create_json/', views.ParserView.as_view()),
 ]
