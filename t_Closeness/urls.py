5578cf17f1a5beb21fc0ffeb3f9ed51e630b7308
bob <cn90111@gmail.com>
2020-05-26 10:22:11 +0800

    hotfix: 刪除多餘的檔案
    調整項目：
    1. 刪除 template 中的 file_list
    
    issue #98

--- t_Closeness/urls.py
+++ t_Closeness/urls.py
@@ -5,13 +5,13 @@ from json_parser import views as json_parser_views
 from . import t_closeness
 
 urlpatterns = [
-	path('',views.index),
-	path('Execute_t_closeness/', t_closeness.t_closeness),
-	path('show_t_closeness/', t_closeness.show_progress),
-	path('Execute_Page/csv_name=<str:csv_name>/', general_views.ExecuteView.as_view()),
-	path('File_List_<str:method>/', general_views.FileListView.as_view()),
-	path('Download_Output/csv_name=<str:csv_name>/', general_views.DownloadView.as_view()),
-	path('CSV_View_<str:method>/', general_views.DisplayCsvView.as_view()),
+    path('',views.index),
+    path('Execute_t_closeness/', t_closeness.t_closeness),
+    path('show_t_closeness/', t_closeness.show_progress),
+    path('Execute_Page/csv_name=<str:csv_name>/', general_views.ExecuteView.as_view()),
+    path('File_List_<str:method>/', general_views.FileListView.as_view()),
+    path('Download_Output/csv_name=<str:csv_name>/', general_views.DownloadView.as_view()),
+    path('CSV_View_<str:method>/', general_views.DisplayCsvView.as_view()),
     path('delete_file_<str:method>/', general_views.DeleteFileView.as_view()),
     path('custom/csv_name=<str:csv_name>/', json_parser_views.CustomView.as_view()),
     path('finish/csv_name=<str:csv_name>/', general_views.FinishView.as_view()),
