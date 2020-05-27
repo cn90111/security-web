4249553299d7084883d13b621866b771ce1cd208
bob <cn90111@gmail.com>
2020-05-27 10:02:13 +0800

    hotfix: 刪除多餘的檔案

--- Template/general/js/exit_page.js
+++
@@ -1,9 +0,0 @@
-<script>
-    window.onbeforeunload = function(e) {
-      var dialogText = '離開此頁面後，檔案與設定皆不會保留，確定要離開嗎?';
-      e.returnValue = dialogText;
-      return dialogText;
-    };
-    
-    
-</script>
