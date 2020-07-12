d94fdba60953d8aebc717f53b40a878e580304b1
bob <cn90111@gmail.com>
2020-07-12 15:13:03 +0800

    feat: 完成以去識別化模型檢測原始檔案
    調整項目：
    1. 調整utility.html使重複內容減少
    2. 調整ML使他可以用不同檔案訓練/驗證
    3. 加入one-hot，使不同檔案能得到統一的編碼
    
    issue #131

--- general/machine_learning.py
+++ general/machine_learning.py
@@ -12,7 +12,7 @@ class MachineLearning():
     train_label = None
     test_label = None
     
-    def __init__(self, method, file_path):
+    def __init__(self, method, train_file_path, test_file_path):
         if method == 'decision_trees':
             self.method_object = tree.DecisionTreeClassifier()
         elif method == 'svm':
@@ -21,14 +21,25 @@ class MachineLearning():
             self.method_object = RandomForestClassifier(max_depth=20, random_state=0)
         else:
             raise AttributeError("無此method：" + method)
-        self.preprocessing(file_path)
+        self.preprocessing(train_file_path, test_file_path)
 
-    def preprocessing(self, file_path):
-        feature, label = self.getDataset(file_path)
-        self.train_feature, self.test_feature = \
-            self.percentage_split(feature, self.train_percent)
-        self.train_label, self.test_label = \
-            self.percentage_split(label, self.train_percent)
+    def preprocessing(self, train_file_path, test_file_path):
+        train_feature, train_label = self.getDataset(train_file_path)
+        test_feature, test_label = self.getDataset(test_file_path)
+        
+        train_feature, test_feature = self.one_hot_encoding(train_feature, test_feature)
+        train_label, test_label = self.one_hot_encoding(train_label, test_label)
+        
+        train_label = [item for sublist in train_label for item in sublist]
+        test_label = [item for sublist in test_label for item in sublist]
+        
+        split_point = len(train_feature)*self.train_percent//100        
+        self.train_feature = train_feature[0:split_point]
+        self.train_label = train_label[0:split_point]
+        
+        split_point = len(test_feature)*self.train_percent//100
+        self.test_feature = test_feature[split_point:]
+        self.test_label = test_label[split_point:]
             
     def fit(self):
         self.method_object.fit(self.train_feature, self.train_label)
@@ -47,15 +58,69 @@ class MachineLearning():
     def getDataset(self, file_path):
         dataframe = pd.read_csv(file_path)
         feature = dataframe.iloc[:, 0:-1]
-        feature = pd.get_dummies(feature).values.tolist()
-        label = dataframe.iloc[:, -1]
-        if type(label) is str or type(label) is chr:
-            label = pd.get_dummies(label).values.tolist()
-        else:
-            label = label.values.tolist()
+        label = dataframe.iloc[:, [-1]]
         return feature, label
         
-    def percentage_split(self, data, percent):
-        split_point = len(data)*percent//100
-        return data[0:split_point], data[split_point:]
+    def one_hot_encoding(self, dataframe1, dataframe2):
+        encoding_dict = self.get_encoding_dict(dataframe1, dataframe2)
+        list1 = self.encoding(dataframe1, encoding_dict)
+        list2 = self.encoding(dataframe2, encoding_dict)
+        return list1, list2
+            
+    def need_one_hot(self, data1, data2):
+        if type(data1) is str or type(data1) is chr:
+            return True
+        if type(data2) is str or type(data2) is chr:
+            return True
+        return False
+
+    def get_elements(self, check_data, dataframe):
+        if type(check_data) is str or type(check_data) is chr:
+            return set(dataframe.values.tolist())
+        else:
+            return set()
+    
+    def get_encoding_dict(self, dataframe1, dataframe2):
+        encoding_dict = {}
+        for column_title in dataframe1:
+            data1 = None
+            data2 = None
+            i = 0
+            while (not data1 or not data2) and (i < dataframe1.shape[0] and i < dataframe2.shape[0]):
+                if not data1:
+                    data1 = dataframe1.loc[i, column_title]
+                if not data2:
+                    data2 = dataframe2.loc[i, column_title]
+                i = i + 1
+            if self.need_one_hot(data1, data2):
+                elements1 = self.get_elements(data1, dataframe1.loc[:, column_title])
+                elements2 = self.get_elements(data2, dataframe2.loc[:, column_title])
+                encoding_dict[column_title] = list(elements1.union(elements2))
+        return encoding_dict
         
+    def encoding(self, dataframe, encoding_dict):
+        encoding_list = []
+        for row in range(dataframe.shape[0]):
+            column_list = []
+            for column_title in dataframe:
+                data = dataframe.loc[row, column_title]
+                if column_title in encoding_dict:
+                    if data not in encoding_dict[column_title]:
+                        data = self.number_pair(data, encoding_dict[column_title])
+                    for element in encoding_dict[column_title]:
+                        if data == element:
+                            column_list.append(1)
+                        else:
+                            column_list.append(0)
+                else:
+                    column_list.append(data)
+            encoding_list.append(column_list.copy())
+        return encoding_list
+    
+    def number_pair(self, number, interval_set):
+        for interval in interval_set:
+            limit = interval.split('-')
+            min = float(limit[0])
+            max = float(limit[1])
+            if number > min and number < max:
+                return interval
