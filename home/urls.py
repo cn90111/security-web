0cba8a750c74a2d3f8a23ba5d3506afb88bfb7b4
bob <cn90111@gmail.com>
2020-08-26 08:52:18 +0800

    hotfix: 修正更新日誌切換語言會引發的錯誤

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from general import views as general_views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('initialize/', views.InitializeView.as_view(), name='initialize'),
+    path('update_log/', general_views.UpdateLogView.as_view(), name = 'update_log'),
 ]
