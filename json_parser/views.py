5a1e90d85a5e02ceefb4bd664e1e81a7564c69ca
bob <cn90111@gmail.com>
2020-09-02 15:50:31 +0800

    hotfix: 修復區間設定錯誤
    錯誤原因：
    1. 取得 DOM 的 value 後沒有進行型態轉換
    
    調整項目：
    1. 後台傳遞欄位的型態給前台以做轉換
    2. 藉由取得的型態，避免自動生成數值區間時變為浮點數
    
    備註：
    1. pandas的read_csv當整數欄位有空值時，會錯誤解讀為float
    2. 新版的pandas有推出Int64，或許可解決該問題
    
    issue #207

--- json_parser/views.py
+++ json_parser/views.py
@@ -107,17 +107,33 @@ class DPViewParserView(View):
         
 class CustomView(View):
     @method_decorator(login_required)
-    def get(self, request, *arg, **kwargs):
-        parser = JsonParser()
-        path = Path()
-        
-        string_element_dict = {} # column_title - element
-        
+    def get(self, request, *arg, **kwargs):        
         file_name = kwargs.get('csv_name')
         if not file_name:
             return redirect('home')
         alert_message = kwargs.get('alert_message')
-            
+         
+        request_dict = self.get_request_dict(request, *arg, **kwargs)
+        request_dict['alert_message'] = alert_message
+        return render(request, 'general/parameter_custom.html', request_dict)
+        
+    @method_decorator(login_required)
+    def post(self, request, *arg, **kwargs):
+        file_name = kwargs.get('csv_name')
+        if not file_name:
+            return redirect('home')
+        title_id_pair = request.POST.get('title_id_pair', None)
+        
+        request_dict = self.get_request_dict(request, *arg, **kwargs)
+        request_dict['title_id_pair'] = title_id_pair
+        return render(request, 'general/parameter_custom.html', request_dict)
+    
+    def get_request_dict(self, request, *arg, **kwargs):
+        parser = JsonParser()
+        path = Path()
+        
+        string_element_dict = {} # column_title - element        
+        file_name = kwargs.get('csv_name')            
         caller = path.get_caller(request)
             
         file_path = path.get_upload_path(request, file_name, caller=caller)
@@ -125,39 +141,12 @@ class CustomView(View):
               
         request_dict = {}  
         request_dict = self.set_url_path(request_dict, caller, file_name)
-        request_dict['alert_message'] = alert_message
         request_dict['string_element_dict'] = string_element_dict
         request_dict['caller'] = caller
         request_dict['file_name'] = file_name
         request_dict['custom_mode'] = 'json_parser'
-        return render(request, 'general/parameter_custom.html', request_dict)
-        
-    @method_decorator(login_required)
-    def post(self, request, *arg, **kwargs):
-        parser = JsonParser()
-        path = Path()
-        
-        string_element_dict = {} # column_title - element
-        
-        file_name = kwargs.get('csv_name')
-        if not file_name:
-            return redirect('home')
-        title_id_pair = request.POST.get('title_id_pair', None)
-            
-        caller = path.get_caller(request)
-            
-        file_path = path.get_upload_path(request, file_name, caller=caller)
-        string_element_dict = parser.get_file_string_element(file_path)
-              
-        request_dict = {}  
-        request_dict = self.set_url_path(request_dict, caller, file_name)        
-        request_dict['string_element_dict'] = string_element_dict
-        request_dict['caller'] = caller
-        request_dict['title_id_pair'] = title_id_pair
-        request_dict['file_name'] = file_name
-        request_dict['custom_mode'] = 'json_parser'
-        return render(request, 'general/parameter_custom.html', request_dict)
-        
+        return request_dict
+    
     def set_url_path(self, request_dict, caller, file_name):
         request_dict['create_json'] = reverse(caller+':create_json')
         request_dict['title_check'] = reverse(caller+':title_check')
@@ -170,62 +159,42 @@ class CustomView(View):
 class AdvancedSettingsView(CustomView):
     @method_decorator(login_required)
     def get(self, request, *arg, **kwargs):
-        parser = JsonParser()
-        path = Path()
-        number_data_frame = NumberDataframe()
-        
-        string_element_dict = {} # column_title - element
-        username = request.user.get_username()
         file_name = kwargs.get('csv_name')
-        alert_message = kwargs.get('alert_message')
+        if not file_name:
+            return redirect('home')
             
-        caller = path.get_caller(request)
-        
-        file_path = path.get_upload_path(request, file_name, caller=caller)
-        string_element_dict = parser.get_file_string_element(file_path)
-        number_title_list = number_data_frame.get_number_title(file_path)
-        max_value_dict, min_value_dict = number_data_frame.get_number_limit(file_path, number_title_list)
-           
-        request_dict = {}
-        request_dict = self.set_url_path(request_dict, caller, file_name)
+        alert_message = kwargs.get('alert_message')
+        request_dict = self.get_request_dict(request, *arg, **kwargs)
         request_dict['alert_message'] = alert_message
-        request_dict['string_element_dict'] = string_element_dict
-        request_dict['file_name'] = file_name
-        request_dict['custom_mode'] = 'json_parser'
-        request_dict['caller'] = caller
-        request_dict['advanced_settings'] = True
-        request_dict['number_title_list'] = number_title_list
-        request_dict['max_value_dict'] = max_value_dict
-        request_dict['min_value_dict'] = min_value_dict
         return render(request, 'general/parameter_custom.html', request_dict)
     
     @method_decorator(login_required)
     def post(self, request, *arg, **kwargs):
-        parser = JsonParser()
-        path = Path()
-        number_data_frame = NumberDataframe()
-        
-        string_element_dict = {} # column_title - element
-        username = request.user.get_username()
         file_name = kwargs.get('csv_name')
-        title_id_pair = request.POST.get('title_id_pair', None)
+        if not file_name:
+            return redirect('home')
             
-        caller = path.get_caller(request)
-        
-        file_path = path.get_upload_path(request, file_name, caller=caller)
-        string_element_dict = parser.get_file_string_element(file_path)
-        number_title_list = number_data_frame.get_number_title(file_path)
-        max_value_dict, min_value_dict = number_data_frame.get_number_limit(file_path, number_title_list)
-           
-        request_dict = {}
-        request_dict = self.set_url_path(request_dict, caller, file_name)  
-        request_dict['string_element_dict'] = string_element_dict
-        request_dict['file_name'] = file_name
-        request_dict['custom_mode'] = 'json_parser'
-        request_dict['caller'] = caller
+        title_id_pair = request.POST.get('title_id_pair', None)            
+        request_dict = self.get_request_dict(request, *arg, **kwargs)
         request_dict['title_id_pair'] = title_id_pair
+        return render(request, 'general/parameter_custom.html', request_dict)
+        
+    def get_request_dict(self, request, *arg, **kwargs):
+        path = Path()
+        
+        file_name = kwargs.get('csv_name')            
+        caller = path.get_caller(request) 
+        
+        file_path = path.get_upload_path(request, file_name, caller=caller)        
+        number_data_frame = NumberDataframe(file_path)
+        number_title_list = number_data_frame.get_number_title()
+        number_type_pair = number_data_frame.get_number_type_pair(number_title_list)
+        max_value_dict, min_value_dict = number_data_frame.get_number_limit(number_title_list, number_type_pair=number_type_pair)
+           
+        request_dict = super().get_request_dict(request, *arg, **kwargs)
         request_dict['advanced_settings'] = True
         request_dict['number_title_list'] = number_title_list
+        request_dict['number_type_pair'] = number_type_pair
         request_dict['max_value_dict'] = max_value_dict
         request_dict['min_value_dict'] = min_value_dict
-        return render(request, 'general/parameter_custom.html', request_dict)
+        return request_dict
